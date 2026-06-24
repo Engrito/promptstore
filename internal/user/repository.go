44d4858dc537d4f9da4ad6a4fb086c6f1e1507c8
Dinesh Kumar <dineshkumawattart@gmail.com>
2026-06-24 21:54:49 +0530

    user repository setup

---
+++ internal/user/repository.go
@@ -0,0 +1,50 @@
+package user
+
+import (
+	"context"
+	"errors"
+	"sync"
+)
+
+type UserRepository interface {
+	Add(ctx context.Context, user User) (ID, error)
+	GetById(ctx context.Context, id ID) (User, error)
+	UpdateByID(ctx context.Context, id ID) error
+	DeleteByID(ctx context.Context, id ID) error
+	GetAll(ctx context.Context, id ID) error
+}
+
+var (
+	ErrNotImplemented = errors.New("not implemented")
+)
+
+type InMermoryUserRepository struct {
+	mu     sync.Mutex
+	ListDB []User
+}
+
+func NewInMermoryUserRepository() *InMermoryUserRepository {
+	return &InMermoryUserRepository{
+		ListDB: make([]User, 0),
+	}
+}
+
+func (ur *InMermoryUserRepository) Add(ctx context.Context, user User) (ID, error) {
+	return "", ErrNotImplemented
+}
+
+func (ur *InMermoryUserRepository) GetById(ctx context.Context, id ID) (User, error) {
+	return User{}, ErrNotImplemented
+}
+
+func (ur *InMermoryUserRepository) UpdateByID(ctx context.Context, id ID) error {
+	return ErrNotImplemented
+}
+
+func (ur *InMermoryUserRepository) DeleteByID(ctx context.Context, id ID) error {
+	return ErrNotImplemented
+}
+
+func (ur *InMermoryUserRepository) GetAll() error {
+	return ErrNotImplemented
+}

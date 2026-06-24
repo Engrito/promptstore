2fccd1a515a2efc91a730df1bed815881b8f08dc
Dinesh Kumar <dineshkumawattart@gmail.com>
2026-06-24 22:50:27 +0530

    user routes setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,22 +2,22 @@ package main
 
 import (
 	"log"
-	"net/http"
 
 	"github.com/engrito/promptstore/internal/user"
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func routes(userHanlders user.UserHandlers) *gin.Engine {
 	r := gin.Default()
+	return r
+}
 
+func main() {
 	userRepo := user.NewInMermoryUserRepository()
 	userService := user.NewService(userRepo)
-	_ := user.NewUserHandlers(*userService)
-	// define simple get endpoint
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{"message": "pong"})
-	})
+	userHandlers := user.NewUserHandlers(*userService)
+
+	r := routes(*userHandlers)
 
 	log.Println("server, http://localhost:8080")
 	err := r.Run()
